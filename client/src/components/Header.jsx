7d1c8bd65c0c4a539b2106bf7d2bf34662a656ee
jayrajgb <jayrajgborate11@gmail.com>
2025-08-11 16:56:16 +0530

    frontend begins - nav & header done

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Sparkles } from "lucide-react";
+import { assets } from "../assets/assets";
+
+const Header = () => {
+  return (
+    <div className="relative mx-8 sm:mx-16 xl:mx-24">
+      <div className="mt-20 mb-8 text-center">
+        {/* AI Feature Button */}
+        <div className="border-primary/40 bg-primary/10 text-primary mb-4 inline-flex items-center justify-center gap-4 rounded-full border px-6 py-2 text-sm">
+          <p>AI Features Integrated</p>
+          <Sparkles size={12} />
+        </div>
+
+        {/* Title */}
+        <h1 className="text-mytext mt-4 text-3xl font-semibold sm:text-6xl sm:leading-16">
+          New Era Of <span className="text-primary">Blogging</span> <br />
+          Globally
+        </h1>
+
+        {/* Paragraph */}
+        <p className="text-mytext m-auto my-6 max-w-2xl max-sm:text-xs sm:my-8">
+          From local thoughts to worldwide impact — a new stage for every
+          storyteller. Explore fresh perspectives, limitless reach — blogging
+          like never before.
+        </p>
+
+        {/* Form */}
+        <form
+          action=""
+          className="border-mytext/10 mx-auto flex max-w-lg justify-between overflow-hidden rounded border bg-white max-sm:scale-75"
+        >
+          <input
+            type="text"
+            placeholder="Search blogs"
+            name=""
+            id=""
+            required
+            className="w-full pl-4 outline-none"
+          />
+          <button
+            type="submit"
+            className="bg-primary m-2 cursor-pointer rounded px-8 py-2 text-white transition-all hover:scale-105"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+      <img
+        src={assets.gradientBackground}
+        alt="bg"
+        className="absolute -top-50 -z-1 opacity-50"
+      />
+    </div>
+  );
+};
+
+export default Header;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+// import { assets } from "../assets/assets";
+import { ArrowRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="mx-8 flex items-center justify-between py-5 sm:mx-20 xl:mx-32">
+      {/* <img src={assets.logo} alt="logo" className="w-32 sm:w-44" /> */}
+      <div
+        className="w-32 cursor-pointer sm:w-44"
+        onClick={() => navigate("/")}
+      >
+        Blogger
+      </div>
+      <button
+        className="bg-primary group flex cursor-pointer items-center gap-2 rounded-full px-10 py-3 text-sm text-white"
+        onClick={() => navigate("/admin")}
+      >
+        Login{" "}
+        <span>
+          <ArrowRight
+            className="transition-all duration-300 group-hover:translate-x-1"
+            size={16}
+          />
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
