051812eaeca46b31c01f06989bc43173fc06afba
jayrajgb <jayrajgborate11@gmail.com>
2025-08-12 18:36:39 +0530

    login page completed

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <div></div>
+    <div className="flex h-screen items-center justify-center">
+      <div className="border-primary/30 shadow-primary/15 w-full max-w-sm rounded-lg border p-6 shadow-xl max-md:m-6">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full py-6 text-center">
+            <h1 className="text-3xl font-bold">
+              <span className="text-primary">Admin</span>
+              Login
+            </h1>
+            <p className="font-light">Enter credentials to access admin!</p>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            className="text-mytext mt-6 w-full sm:max-w-md"
+          >
+            <div className="flex flex-col">
+              <label htmlFor="">Email</label>
+              <input
+                className="border-mytext/20 mb-6 border-b-2 p-2 outline-none"
+                type="email"
+                placeholder="Enter email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Password</label>
+              <input
+                className="border-mytext/20 mb-6 border-b-2 p-2 outline-none"
+                type="password"
+                placeholder="Enter password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-primary/80 hover:bg-primary w-full cursor-pointer rounded py-3 font-medium text-white transition-all"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
