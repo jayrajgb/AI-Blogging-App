6429518daba400887268b7c8eea6b23756d248de
jayrajgb <jayrajgborate11@gmail.com>
2025-08-14 13:44:06 +0530

    jwt login added

---
+++ server/controllers/blogController.js
@@ -0,0 +1,4 @@
+export const addBlog = async (req, res) => {
+  try {
+  } catch (error) {}
+};
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 const app = express();
 
@@ -14,10 +15,12 @@ app.get("/", (req, res) => {
   res.send("API started!");
 });
 
+app.use("/api/admin", adminRouter);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server is running at PORT:", PORT);
+  console.log("Server is running at: http://localhost:" + PORT + "/");
 });
 
 export default app;
