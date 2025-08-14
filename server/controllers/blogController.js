c049f3fd1b6ecbf461697abef5962b54e3038408
jayrajgb <jayrajgborate11@gmail.com>
2025-08-14 15:30:21 +0530

    blog api added

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,4 +1,50 @@
+import fs from "fs";
+import imagekit from "../config/imagekit.js";
+import blogModel from "../models/blogModel.js";
+
 export const addBlog = async (req, res) => {
   try {
-  } catch (error) {}
+    const { title, subtitle, description, category, isPublished } = JSON.parse(
+      req.body.blog
+    );
+    const img = req.file;
+
+    if (!title || !description || !category || !img) {
+      return res.json({ success: false, message: "Incomplete info!" });
+    }
+
+    const fileBuffer = fs.readFileSync(img.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: img.originalname,
+      folder: "/blogs",
+    });
+
+    const optimizedImgUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        {
+          quality: "auto",
+          format: "webp",
+          width: "1280",
+        },
+      ],
+    });
+
+    const image = optimizedImgUrl;
+
+    await blogModel.create({
+      title,
+      subtitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json({ succes: true, message: "Blog added successfully!" });
+  } catch (error) {
+    res.json({ succes: false, message: error.message });
+    console.log(error.message);
+  }
 };
